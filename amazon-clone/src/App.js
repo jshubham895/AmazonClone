8dc371470252f204a059d30bb2900b25c57419a7
jshubham895 <jshubham895@gmail.com>
2021-09-07 22:53:31 +0530

    fifth

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Shubham</h1>
       {/* HEADER */}
       <Header />
+      <Home />
       {/* HOME */}
     </div>
   );
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/All_Or_Nothing_Tottenham_Hotspur_S1/AONT_S1_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB406302419_.jpg"
+          alt=""
+          className="home__image"
+        />
+
+        <div className="home__row">
+          <Product />
+          {/* {Product} */}
+        </div>
+
+        <div className="home__row">
+          {/* {Product} */}
+          {/* {Product} */}
+          {/* {Product} */}
+        </div>
+
+        <div className="home__row">{/* {Product} */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
