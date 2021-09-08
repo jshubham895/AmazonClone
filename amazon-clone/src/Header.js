3223884640aa283bee4cdbbc582ad4c43f3ae97b
jshubham895 <jshubham895@gmail.com>
2021-09-08 12:41:22 +0530

    checkout page

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
 	return (
 		<div className="header">
-			<img
-				className="header__logo"
-				src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-				alt=""
-			/>
-
+			<Link to="/">
+				<img
+					className="header__logo"
+					src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+					alt=""
+				/>
+			</Link>
 			<div className="header__search">
 				<input className="header__searchInput" type="text" />
 				<SearchIcon className="header__searchIcon" />
@@ -30,10 +32,12 @@ function Header() {
 					<span className="header__OptionLineOne">Your</span>
 					<span className="header__OptionLineTwo">Prime</span>
 				</div>
-				<div className="header__optionBasket">
-					<ShoppingBasketIcon />
-					<span className="header__optionLineTwo header__basketCount">0</span>
-				</div>
+				<Link to="/checkout">
+					<div className="header__optionBasket">
+						<ShoppingBasketIcon />
+						<span className="header__optionLineTwo header__basketCount">0</span>
+					</div>
+				</Link>
 			</div>
 		</div>
 	);
